a5e32a34fa2a500f705dd818e6385b661f39c5bc
Julyna <lingna.chen@outlook.com>
2018-10-17 12:17:28 +0800

    todo list

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # vue_demo
 
-> my first vue demo
+> A Vue.js project
 
 ## Build Setup
 
@@ -16,8 +16,15 @@ npm run build
 
 # build for production and view the bundle analyzer report
 npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
->项目介绍
-* 采用vue-cli 构建初始项目目录
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,78 @@
 <template>
+  <!-- vue模板里面，所有内容要被一个根节点包含起来 -->
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- 这是一个根组件 -->
+    <input type="text" v-model="todo" @keydown="doAdd($event)">
+    <!-- <button @click="doAdd()">+增加</button> -->
+    <hr>
+    <h2>进行中</h2>
+    <ul>
+      <li v-for="(item,key) in list" v-if="!item.checked">
+        <input type="checkbox" v-model="item.checked" @change="saveList()">
+        {{item.title}}
+        <button @click="removeData(key)">删除</button>
+      </li>
+    </ul>
+    
+    <hr>
+    <h2>已完成</h2>
+    <ul>
+      <li v-for="(item,key) in list" v-if="item.checked">
+        <input type="checkbox" v-model="item.checked" @change="saveList()">
+        {{item.title}}
+        <button @click="removeData(key)">删除</button>
+      </li>
+    </ul>
+
   </div>
 </template>
 
 <script>
+      // [
+      //   {
+      //     title:'录制1111',
+      //     checked:true
+      //   },
+      //   {
+      //     title:'录制2222',
+      //     checked:true
+      //   }
+      // ]
 export default {
-  name: 'App'
+  name: 'App',
+  data(){  //业务逻辑里定义的数据
+    return{
+      todo:'',
+      list: []
+    }
+  },
+  methods:{
+    doAdd(e){
+      console.log(e.keyCode);
+      if(e.keyCode==13){
+        this.list.push({
+          title:this.todo,
+          checked:false,  //待办事项
+        });
+        this.todo='';     
+      }
+      localStorage.setItem('list',JSON.stringify(this.list))
+    },
+    removeData(key){
+      this.list.splice(key,1);
+      localStorage.setItem('list',JSON.stringify(this.list))
+    },
+    saveList(){
+      localStorage.setItem('list',JSON.stringify(this.list))
+    }
+  },
+  mounted(){  //生命周期函数，vue页面刷新就会触发的方法
+    var list=JSON.parse(localStorage.getItem('list'));
+    if(list){  //注意判断
+      this.list=list;
+    }
+  }
+ 
 }
 </script>
 
@@ -20,4 +85,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.finish li {
+  background-color: #eee;
+}
+
 </style>
